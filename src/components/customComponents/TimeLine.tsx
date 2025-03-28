23d09750c8834a46cf8f92a8e79e466a1e070195
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2025-03-28 11:20:19 +0530

    refactor: styling changes in badge, heading, pricing

--- src/components/customComponents/TimeLine.tsx
+++ src/components/customComponents/TimeLine.tsx
@@ -20,20 +20,22 @@ export function TimelineDemo() {
                   className="mb-8 p-4 border border-gray-700 rounded-lg shadow-md relative bg-black"
                 >
                   {/* Badge */}
-                  <div className="absolute top-0 right-0 bg-red-500 text-white text-xs font-bold py-1 px-2 rounded-bl-lg">
+                  <div className="absolute top-0 right-0 bg-gradient-to-r from-purple-600 to-pink-600 text-white text-xs font-bold py-1 px-2 rounded-bl-lg">
                     {capsule.badge}
                   </div>
 
-                  <h3 className="text-lg md:text-xl font-semibold mb-2 mt-2 text-white">
-                    <div className="font-bold text-3xl">{capsule.name}</div>
+                  <h3 className="text-lg md:text-xl font-semibold mb-2 mt-4 text-white">
+                  <div className="font-bold text-3xl bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
+  {capsule.name}
+</div>
                   </h3>
                   <p className="text-md md:text-lg font-semibold mb-2 text-gray-300">
                     <span className="font-bold">Key Outcome:</span> {capsule.outcome}
                   </p>
                   <p className="text-md md:text-lg font-medium mb-4 text-gray-300">
-                    Duration: {capsule.duration} <span className="pl-20">Price: ₹ {capsule.price}</span>
+                    Duration: {capsule.duration} <span className="px-2 ml-20 bg-gray-800 gradient-border">Price: ₹ {capsule.price}</span>
                   </p>
-                  <div className="grid grid-cols-2 gap-4">
+                  {/* <div className="grid grid-cols-2 gap-4">
                     {capsule.images.map((image, imgIndex) => (
                       <Image
                         key={imgIndex}
@@ -44,7 +46,7 @@ export function TimelineDemo() {
                         className="rounded-lg bg-white object-cover h-40 md:h-60 w-full shadow-lg"
                       />
                     ))}
-                  </div>
+                  </div> */}
                 </div>
               ))}
             </div>
