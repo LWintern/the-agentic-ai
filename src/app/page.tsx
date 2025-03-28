e8abd1f9849561ea4b383e503b13de124b2b5e25
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2025-03-28 10:59:16 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
    <>
    <HeroSection /> 
-   <App />
+   {/* <App /> */}
    {/* <CardLayout /> */}
    <TimelineDemo />
    {/* <ProjectsSection/>   */}
